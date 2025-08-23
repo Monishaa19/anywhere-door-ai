1e2f4caed0d510a61b3c3fedc4e65231ac1926df
monisha <thenameismonisha@gmail.com>
2025-08-23 15:34:08 +0530

    allowed render

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    allowedHosts: [
+      'anywhere-door-fysp.onrender.com' // ✅ Add your Render domain here
+    ],
     host: "::",
     port: 8080,
   },
